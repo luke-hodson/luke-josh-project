8278a8e1b9971fc1d43c4f71db43302a41078507
hodsonmail <hodsonmail@gmail.com>
2018-08-30 20:40:23 +0100

    Start of auth code

---
+++ luke-josh-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(luke_josh_project.Startup))]
+namespace luke_josh_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
